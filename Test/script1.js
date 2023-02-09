2c1c0f3941e775c0ac2465b5164a0a1166c41cea
George <alximik15@gmail.com>
2023-02-09 14:38:51 +0400

    uploaded info doesn't dissapear after refresh

--- Test/script1.js
+++ Test/script1.js
@@ -1,32 +1,27 @@
 function displayData() {
-    var name = document.getElementById("name").value;
-    var surname = document.getElementById("surname").value;
-    var aboutMe = document.getElementById("aboutMe").value;
-    var email = document.getElementById("email").value;
-    var phoneNumber = document.getElementById("phoneNumber").value;
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var aboutMe = document.getElementById("aboutMe").value;
+  var email = document.getElementById("email").value;
+  var phoneNumber = document.getElementById("phoneNumber").value;
 
-    document.getElementById("display").innerHTML =
-      `${name} ${surname}<br><br>
+  localStorage.setItem("name", name);
+  localStorage.setItem("surname", surname);
+  localStorage.setItem("aboutMe", aboutMe);
+  localStorage.setItem("email", email);
+  localStorage.setItem("phoneNumber", phoneNumber);
+
+  document.getElementById("display").innerHTML =
+    `${name} ${surname}<br><br>
       
       ${email}<br>
       ${phoneNumber}<br><br>
       ჩემს შესახებ <br> <br>
       
-      ${aboutMe}`
-  }
+      ${aboutMe}`;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-  const inputField = document.getElementById("image");
+const inputField = document.getElementById("image");
 const outputImage = document.getElementById("output-image");
 
 inputField.addEventListener("change", function() {
@@ -41,7 +36,16 @@ inputField.addEventListener("change", function() {
 
   reader.onload = function() {
     outputImage.src = reader.result;
+    localStorage.setItem("image", reader.result);
   };
 });
 
-  
+// Get the data from local storage and populate the fields when the page loads
+window.onload = function() {
+  document.getElementById("name").value = localStorage.getItem("name");
+  document.getElementById("surname").value = localStorage.getItem("surname");
+  document.getElementById("aboutMe").value = localStorage.getItem("aboutMe");
+  document.getElementById("email").value = localStorage.getItem("email");
+  document.getElementById("phoneNumber").value = localStorage.getItem("phoneNumber");
+  document.getElementById("output-image").src = localStorage.getItem("image");
+};
